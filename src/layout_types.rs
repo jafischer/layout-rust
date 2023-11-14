51be54bba51e13cda3cada199a9cd0bbb869b88b
Jonathan Fischer <jonathan.fischer@sony.com>
2023-11-14 10:59:33 -0800

    Added `MaybeRegex` class to handle both exact and regex matching for window names. Renamed WindowLayout & ScreenLayout to *Info. Added vector of process_id and window_id to handle multiple windows with same name.

--- src/layout_types.rs
+++ src/layout_types.rs
@@ -1,33 +1,77 @@
-use crate::layout_types::MaybeRegex::{Exact, Wildcard};
-use core_graphics_types::base::CGFloat;
+use crate::layout_types::MaybeRegex::{Exact, RE};
 use core_graphics_types::geometry::{CGPoint, CGRect, CGSize};
 use regex::Regex;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Layout {
-    pub screens: Vec<ScreenLayout>,
-    pub windows: Vec<WindowLayout>,
+    pub screens: Vec<ScreenInfo>,
+    pub windows: Vec<WindowInfo>,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct ScreenLayout {
+pub struct ScreenInfo {
     pub id: u32,
     pub frame: Rect,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct WindowLayout {
-    pub owner_name: String,
-    pub name: String,
-    pub display_id: u32,
+pub struct WindowInfo {
+    pub owner_name: MaybeRegex,
+    pub name: MaybeRegex,
+    // For our purposes we want to move every window that has the same Owner Name + Name to the same position. So
+    // we need to keep track of the process_id + window_id of all matching windows.
+    #[serde(skip_serializing, skip_deserializing)]
+    pub ids: Vec<WindowIds>,
+    pub screen_num: usize,
     pub bounds: Rect,
+}
+
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+pub struct Rect {
+    pub x: i32,
+    pub y: i32,
+    pub w: i32,
+    pub h: i32,
+}
+
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
+pub struct WindowIds {
+    pub process_id: i32,
+    pub window_id: u32,
+}
+
+impl WindowInfo {
+    pub fn matches(&self, other: &Self) -> bool {
+        (self.owner_name.matches(&other.owner_name.to_string())
+            && self.name.matches(&other.name.to_string()))
+            || (other.owner_name.matches(&self.owner_name.to_string())
+                && other.name.matches(&self.name.to_string()))
+    }
 }
 
 #[derive(Debug, Clone)]
 pub enum MaybeRegex {
     Exact(String),
-    Wildcard(Regex),
+    RE(Regex),
+}
+
+impl MaybeRegex {
+    pub fn matches(&self, value: &str) -> bool {
+        match self {
+            Exact(s) => s.eq(value),
+            RE(r) => r.is_match(value),
+        }
+    }
+}
+
+impl ToString for MaybeRegex {
+    fn to_string(&self) -> String {
+        match self {
+            Exact(value) => value.clone(),
+            RE(value) => value.as_str().to_string(),
+        }
+    }
 }
 
 impl serde::Serialize for MaybeRegex {
@@ -37,7 +81,7 @@ impl serde::Serialize for MaybeRegex {
     {
         match self {
             Exact(value) => serializer.serialize_str(value),
-            Wildcard(value) => serializer.serialize_str(value.as_str()),
+            RE(value) => serializer.serialize_str(value.as_str()),
         }
     }
 }
@@ -50,7 +94,7 @@ impl<'de> serde::Deserialize<'de> for MaybeRegex {
         let text_val = String::deserialize(deserializer)?;
 
         Ok(match Regex::new(&text_val) {
-            Ok(value) => Wildcard(value),
+            Ok(value) => RE(value),
             Err(_) => Exact(text_val),
         })
     }
@@ -62,12 +106,11 @@ impl Default for MaybeRegex {
     }
 }
 
-#[derive(Debug, Clone, Default)]
-pub struct Rect {
-    pub x: i32,
-    pub y: i32,
-    pub w: i32,
-    pub h: i32,
+impl Rect {
+    // I only care if the origin is inside the rect, not the whole rect being contained.
+    pub(crate) fn contains_origin(&self, p0: &Rect) -> bool {
+        p0.x >= self.x && p0.x < self.x + self.w && p0.y >= self.y && p0.y < self.y + self.h
+    }
 }
 
 // This is silly, I know, but I'm implementing custom serialization just so that the bounds can be printed on one line
@@ -119,21 +162,6 @@ impl Into<CGRect> for Rect {
                 height: self.h.into(),
             },
         }
-    }
-}
-
-impl Rect {
-    #[allow(unused)]
-    pub fn origin(&self) -> CGPoint {
-        CGPoint {
-            x: self.x as CGFloat,
-            y: self.y as CGFloat,
-        }
-    }
-
-    // I only care if the origin is inside the rect, not the whole rect being contained.
-    pub(crate) fn contains(&self, p0: &&Rect) -> bool {
-        p0.x >= self.x && p0.x < self.x + self.w && p0.y >= self.y && p0.y < self.y + self.h
     }
 }
 
