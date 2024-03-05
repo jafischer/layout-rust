8099c22a73d94824a03fe0baffd00285dd40c80c
Jonathan Fischer <jonathan.fischer@gmail.com>
2024-03-05 10:48:35 -0800

    Usage tweaks.

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,14 @@ use log::LevelFilter;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+/// A tool for restoring your carefully-arranged window layout on your MacBook.
 pub struct Args {
-    /// Save the current layout (prints to stdout). If not specified, then the layout in ~/.layout.yaml will be loaded.
+    /// Save the current layout (prints to stdout). If --save is not specified,
+    /// then the layout in ~/.layout.yaml will be restored.
     #[arg(short, long)]
     pub save: bool,
 
     /// Logging level. Default: Info. Valid values: Off, Error, Warn, Info, Debug, Trace,
-    #[arg(short, long, default_value = "Info")]
+    #[arg(short, long, default_value = "info")]
     pub log_level: LevelFilter,
 }
